36883116ed61702c46698347dc6a1a22712c03d1
Carl0sss <rafailan99@gmail.com>
2022-06-19 21:14:54 -0600

    CRUD COMPLETO Y FUNCIONAL(Sujeto a cambios de ser necesario)

--- django_init/settings.py
+++ django_init/settings.py
@@ -8,6 +8,7 @@ https://docs.djangoproject.com/en/4.0/topics/settings/
 
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
+
 """
 
 from pathlib import Path
@@ -85,12 +86,14 @@ WSGI_APPLICATION = 'django_init.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
-"""DATABASES = {
+"""
+DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
-}"""
+}
+"""
 
 DATABASES = {
     'default': {
